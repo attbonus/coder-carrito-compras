9d313d0843ccb9ab441e8b42ea03b8ca93d337fb
attbonus <attbonusdivine@gmail.com>
2022-11-14 22:40:01 -0600

    primer entrega v2

--- miarchivo.js
+++ miarchivo.js
@@ -1,14 +1,17 @@
-let agregadoDeProductos = prompt("Agregar articulos introducciendo su numero de referencia para calcular el precio a pagar");
-
-
 
 function calculoEspecias(agregadoDeProductos){
-     if(agregadoDeProductos !="")
+    let sum = 0;
+    while(true){
+        let agregadoDeProductos = +prompt("Agregar articulos introducciendo su numero correspondiente para calcular el precio a pagar: Lechuga. Costo: $3 Carne de res. Costo: $7, Mantequilla. Costo: $2, Coca cola. Costo: $3, Pan para hamburguesa. Costo $4");
+        if(!agregadoDeProductos) break;
+        
+        sum += agregadoDeProductos;
+        
+    }
+    return alert("Suma total de sus productos son: " + sum + " Gracias por su compra");
 }
 
-    
-
-    
 
 
 
+calculoEspecias()
