c4a1c59d74acf137304cc555ade7f7d72ac7423c
attbonus <attbonusdivine@gmail.com>
2022-11-14 20:48:17 -0600

    primer entrega

---
+++ miarchivo.js
@@ -0,0 +1,14 @@
+let agregadoDeProductos = prompt("Agregar articulos introducciendo su numero de referencia para calcular el precio a pagar");
+
+
+
+function calculoEspecias(agregadoDeProductos){
+     if(agregadoDeProductos !="")
+}
+
+    
+
+    
+
+
+
