d5c70f7628fb486a697479cb0c098e9b00568e00
attbonus <attbonusdivine@gmail.com>
2022-12-19 22:38:35 -0600

    PreEntrega3+Sarmiento

--- miarchivo.js
+++ miarchivo.js
@@ -1,76 +1,197 @@
-//Se creo un algoritmo para calcular el precio total de los productos del carrito de compras
-function calculoEspecias(agregadoDeProductos) {
-    let sum = 0;
-    while (true) {
-        let agregadoDeProductos = +prompt("Agregar articulos introducciendo su numero correspondiente para calcular el precio a pagar: Lechuga. Costo: $3 Carne de res. Costo: $7, Mantequilla. Costo: $2, Coca cola. Costo: $3, Pan para hamburguesa. Costo $4");
-        if (!agregadoDeProductos) break;
+document.addEventListener('DOMContentLoaded', () => {
 
-        sum += agregadoDeProductos;
+    
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'Maruchan',
+            precio: 12,
+            imagen: 'https://images.unsplash.com/photo-1543806053-d4617b244862?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGF6YSUyMGRlJTIwZmlkZW9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+        },
+        {
+            id: 2,
+            nombre: 'Tomates',
+            precio: 14.5,
+            imagen: 'https://images.unsplash.com/photo-1594057687713-5fd14eed1c17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dG9tYXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+        },
+        {
+            id: 3,
+            nombre: 'Jabon',
+            precio: 19,
+            imagen: 'https://images.unsplash.com/photo-1589060040782-234fa4ee0b61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8amFib258ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        },
+        {
+            id: 4,
+            nombre: 'Gomitas',
+            precio: 32,
+            imagen: 'https://images.unsplash.com/photo-1616536814420-4e9702f83f75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8amFib25mcmVzYXN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        },
+        {
+            id: 5,
+            nombre: 'Tortillas',
+            precio: 22,
+            imagen: 'https://images.unsplash.com/photo-1545505005-0a09f804dcf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dG9ydGlsbGFzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'                        
+        },
+        {
+            id: 6,
+            nombre: 'Tasajo',
+            precio: 60,
+            imagen: 'https://images.unsplash.com/photo-1652209695374-7a91c243f12f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHRhc2Fqb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        },
+
+    ];
+
+    let carrito = [];
+    const divisa = '$';
+    const DOMitems = document.querySelector('#items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    const miLocalStorage = window.localStorage;
+
+    
+
+   
+    function renderizarProductos() {
+        baseDeDatos.forEach((info) => {
+           
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4');
+            
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            
+            const miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.textContent = info.nombre;
+            
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid');
+            miNodoImagen.setAttribute('src', info.imagen);
+            
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
+             
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.textContent = '+';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo);
+        });
+    }
+
+    
+    function anyadirProductoAlCarrito(evento) {
+        
+        carrito.push(evento.target.getAttribute('marcador'))
+        
+        renderizarCarrito();
+        
+        guardarCarritoEnLocalStorage();
+    }
+
+    
+    function renderizarCarrito() {
+       
+        DOMcarrito.textContent = '';
+        
+        const carritoSinDuplicados = [...new Set(carrito)];
+        
+        carritoSinDuplicados.forEach((item) => {
+           
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                
+                return itemBaseDatos.id === parseInt(item);
+            });
+            
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+               
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+            
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+       
+        DOMtotal.textContent = calcularTotal();
+    }
+
+    
+    function borrarItemCarrito(evento) {
+        
+        const id = evento.target.dataset.item;
+      
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        
+        renderizarCarrito();
+      
+        guardarCarritoEnLocalStorage();
 
     }
-    return alert("Suma total de sus productos son: $" + sum + " Gracias por su compra");
-}
 
-calculoEspecias()
+    
+    function calcularTotal() {
+        
+        return carrito.reduce((total, item) => {
+            
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            
+            return total + miItem[0].precio;
+        }, 0).toFixed(2);
+    }
 
-//productos que les sera agregado el descuento por incio de nueva temporada navideña
+   
 
-let productosseleccionados = [ {
-    "nombre": "harina",
-    "precio": 25,
-    "descuento": true,
- }, {
-    "nombre": "jamon",
-    "precio": 45,
-    "descuento": true,
-}, {
-    "nombre": "azucar",
-    "precio": 35,
-    "descuento": false,
-},{
-    "nombre": "torta",
-    "precio": 2,
-    "descuento": false,
-}]
+    function vaciarCarrito() {
+        
+        carrito = [];
+        
+        renderizarCarrito();
+        
+        localStorage.clear();
 
+    }
 
-let productosConDescuentos = productosseleccionados.filter(productosConDescuento => productosConDescuento.descuento === true)
-console.log(productosConDescuentos)
+    function guardarCarritoEnLocalStorage () {
+        miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    }
 
+    function cargarCarritoDeLocalStorage () {
+        
+        if (miLocalStorage.getItem('carrito') !== null) {
+           
+            carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+        }
+    }
 
-
-
-
-
-
-
-
-
-/*
-Procesos necesarios en el carrito de compras tales como: agregado de nuevo producto, cálculo de descuento, suma de la canasta total, elimina productos.
-Así como los elementos necesarios para su  modificacion si es necesaria
-
-*/
-// definir el objeto general para el agregado de un nuevo producto por la persona encargada:
-
-function Producto(nombre, identificador, precioUnitario, cantidad, marca, seccion) {
-    this.nombre = nombre;
-    this.identificador = identificador;
-    this.precioUnitario = precioUnitario;
-    this.cantidad = cantidad;
-    this.marca = marca;
-    this.seccion = seccion;
-    this.descripcion = function () {
-        console.log(`${this.nombre} de la marca ${this.marca} tiene el #ID correspondiente ${this.identificador}, su costo en moneda nacional(MX) es ${this.precioUnitario} y el stock disponible es de ${this.cantidad} piezas, perteneciente a la seccion de: ${this.seccion}`)
-    };
-};
+    
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 
 
-
-const zote = new Producto("zote", "axsdc11", 34.5, 4, "morena", "limpieza General");
-const papelHigienico = new Producto("papel higienico", "xdw342",45.99,12,"Kleenex","cuidado Personal")
-
-
-
-console.log(zote.descripcion())
-console.log(papelHigienico.descripcion())
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    renderizarCarrito();
+});
